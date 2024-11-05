0f1adc25ba21324c936698834c5020fdb5612aa4
Raynell2005 <littlefieldraynell@gmail.com>
2024-11-05 02:48:40 -0800

    Add files via upload

---
+++ ZooKeeperChallenge.cpp
@@ -0,0 +1,50 @@
+// ZooKeeperChallenge.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+int main()
+// String '('Hyena Habitat')'
+Hy01; 4 year old female hyena named Kamari, born in summer, birth date Jun 24 2021, brown color, 140 pounds, from Friguia Park
+Hy02; 12 year old male hyena named Ed, born in fall, birth date Oct 5 2012, brown color, 150 pounds, from Friguia Park,
+Tunisia
+Hy03; 4 year old male hyena named Bud, born in spring, birth date Mar 26 2021, black color, 120 pounds, from Friguia Park,
+Tunisia
+Hy04; 8 year old female hyena named Wema, unknown birth season, birthdate 2016, black and tan striped color, 105
+pounds, from Friguia Park, Tunisia
+// String '('Lion Habitat')'
+6 year old female lion named Kiara, born in spring, birthdate Apr 15 2018, tan color, 300 pounds, from Zanzibar,
+Tanzania
+12 year old female lion named Nala, born in winter, birthdate Feb 22 2012, dark tan color, 375 pounds, from KopeLion,
+Tanzania
+22 year old male lion named Scar, born in fall, birthdate Sept 25 2002, golden color, 450 pounds, from Zanzibar,
+Tanzania
+4 year old female lion named Elsa, born in spring, birthdate May 1 2020, tan and brown color, 275 pounds, from
+KopeLion, Tanzania
+// String '('Tiger Habitat')'
+2 year old male tiger named Tony, born in spring, birthdate May 23 2022, gold and tan stripes color, 270 pounds, from
+Dhaka, Bangladesh
+4 year old female tiger named Amber, born in spring, birthdate Apr 19 2020, black stripes color, 400 pounds, from
+Dhaka, Bangladesh
+18 year old male tiger named Rayas, born in fall, birthdate Nov 15 2006, gold and tan color, 300 pounds, from Bardia,
+Nepal
+3 year old female tiger named Cuddles, born in spring, birthdate May 22 2021, black stripes color, 285 pounds, from
+Bardia, Nepal
+// String '('Bear Habitat')'
+7 year old male bear named Yogi, born in spring, birthdate May 25 2017, brown color, 320 pounds, from Alaska Zoo,
+Alaska
+25 year old female bear named Snuggles, born in spring, Jan 20 1999, black color, 425 pounds, from Woodland
+park Zoo, Washington
+4 year old female bear named Baloo, born in fall, Oct 13 2020, black color, 355 pounds, from Woodland park
+Zoo, Washington
+4 year old male bear named Rupert, born in spring, Feb 3 2020, brown color, 405 pounds, from Alaska Zoo,
+Alaska
+}
+
+//Close the file.
+MyFile.close(ZooKeeperChallenge);
